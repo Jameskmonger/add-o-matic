5d4508ec5699dcbd62b69c23bea3106496538132
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-03-29 13:35:28 +0100

    Add second test

--- test.ts
+++ test.ts
@@ -10,3 +10,12 @@ test('(a, b) should return a + b', (t, a, b, expected) => {
     [ 5, 10, 15 ],
     [ 200, 315, 515 ]
 ]);
+
+test('(a)(b) should return a + b', (t, a, b, expected) => {
+
+}, [
+    [ 1, 1, 2 ],
+    [ 2, 2, 4 ],
+    [ 5, 10, 15 ],
+    [ 200, 315, 515 ]
+]);
