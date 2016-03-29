966f200cb92ac1f05fae11c5345784c020dd22bb
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-03-29 13:35:11 +0100

    Add first test

--- test.ts
+++ test.ts
@@ -1,3 +1,12 @@
 /// <reference path="./modunit.d.ts"/>
 
 import test = require('modunit');
+
+test('(a, b) should return a + b', (t, a, b, expected) => {
+
+}, [
+    [ 1, 1, 2 ],
+    [ 2, 2, 4 ],
+    [ 5, 10, 15 ],
+    [ 200, 315, 515 ]
+]);
